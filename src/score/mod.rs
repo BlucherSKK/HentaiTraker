e6575da48513ba454300578432ab1bcc2a41e020
BlucherSKK <rus.bondarenko2007@gmail.com>
2026-05-02 10:40:30 +0300

    add handling scores statets

--- src/score/mod.rs
+++ src/score/mod.rs
@@ -1,12 +1,32 @@
+use crate::score::torent::TorentState;
+
 mod torent;
 
 pub struct ScoreHandler{
     entitys: Vec<ScoreEntyti>,
 }
 
-pub trait Scorable {
+pub enum ScoreEfect {
+    Increment,
+    Decriment,
+    FullDecriment,
+    Nothing,
+    Shtraff   // бля каккто подебильному штраф на англиском будет поэтому транслит
+}
 
+pub enum NextEvent {
+    Torent(TorentState)
+}
+
+pub trait Scorable {
+    fn get_score(self) -> u32;
+    fn next(self, event: NextEvent) -> (ScoreEfect, u32);
 }
 
 
+
 struct ScoreEntyti<T: Scorable>(T);
+
+impl ScoreHandler {
+    fn from_row_db_str
+}
